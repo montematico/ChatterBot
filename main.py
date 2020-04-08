1748ea88509843fdb2e64db2655f7937fc737467
montematico <montematico@gmail.com>
2020-04-07 23:15:46 -0400

    more testing

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import discord
+import json
+from chatterbot import ChatBot
+
+
 
 client = discord.Client()
+chatbot = ChatBot(
+    'Daryyl',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='sqlite:///database.db'
+)
+
+#trainer = ListTrainer(chatbot)
+
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Darryl is online {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user or message.author.role.name == "Bot":
         return
+    if message.content.startswith('^'):
+        await message.channel.send(chatbot.get_response(message.content))
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('Njk3MjQzODYwMzIwOTExNDg0.Xo0dWw.ATljk0W2XTr4J8rpjk9VRg4693Y')
+client.run("Njk3MjQzODYwMzIwOTExNDg0.Xo0dWw.ATljk0W2XTr4J8rpjk9VRg4693Y")
