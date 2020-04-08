ce410ae13b798cb45447d5f59f91fc56376f7e0d
montematico <montematico@gmail.com>
2020-04-08 00:43:14 -0400

    Added Channel Checking

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import discord
 import json
 from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
+# read file
+with open('config.json', 'r') as myfile:
+    data=myfile.read()
+# parse file
+config = json.loads(data)
 
 
 client = discord.Client()
 chatbot = ChatBot(
-    'Daryyl',
+    config["name"],
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
+        #'chatterbot.logic.TimeLogicAdapter',
         'chatterbot.logic.BestMatch'
     ],
     database_uri='sqlite:///database.db'
 )
 
-#trainer = ListTrainer(chatbot)
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train('chatterbot.corpus.english')
 
 
 @client.event
@@ -25,9 +33,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user or message.author.role.name == "Bot":
-        return
-    if message.content.startswith('^'):
-        await message.channel.send(chatbot.get_response(message.content))
+    print(message.channel)
+    if str(message.channel) == "darrylsvoice":
+        print("string")
+    else:
+        print("not a string")
 
-client.run("Njk3MjQzODYwMzIwOTExNDg0.Xo0dWw.ATljk0W2XTr4J8rpjk9VRg4693Y")
+    if message.author == client.user:
+        return
+
+    if message.channel.name == "daryylsvoice":
+        await message.channel.send(chatbot.get_response(message.content))
+        print(message.content)
+
+    if message.content.startswith('^'):
+        await message.channel.send(chatbot.get_response(message.content[1:]))
+        print(message.content[1:])
+
+    if True: #set this to true to make it learn from all other channels but not respond
+        response = chatbot.get_response(message.content)
+        if False: #set this to true to make it respond to messages in all channels
+            message.channel.send(response)
+
+client.run(config["token"])
